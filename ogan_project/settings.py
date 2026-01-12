dd78f4296d812ca19f4be676336777533be29b6b
AsherwL <weleasher@gmail.com>
2026-01-12 21:29:45 +0100

    Integrate Cloudinary for media storage

--- ogan_project/settings.py
+++ ogan_project/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'core',
     'store',
     'orders',
@@ -132,6 +134,15 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # CART_SESSION_ID = 'cart'
 CART_SESSION_ID = 'cart'
+
+# Cloudinary Storage Settings
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET')
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Static files storage
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
